5404490e1233ed931dd62981bca8ffa462a0cd31
DaniLott1988 <danilott1988@gmail.com>
2022-02-22 23:12:57 +0100

    Task 5.1V02

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native-web';
+import React from 'react';
+import Start from './components/Start';
+import Chat from './components/Chat';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+
+  render() {
+
+    return (
+
+      <NavigationContainer>
+
+        <Stack.Navigator initialRouteName='Start'>
+
+          <Stack.Screen name='Start'
+          component= {Start} />
+
+          <Stack.Screen name='Chat'
+          component= {Chat} />
+
+        </Stack.Navigator>
+
+      </NavigationContainer>
+      
+    );
+  }
 }
 
+//StyleSheets should be placed at the bottom of the files.
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +42,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ */
